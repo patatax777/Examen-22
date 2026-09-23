2710d615b8a98e6ad6ec93d3f520001d38e5e337
patatax777 <kmelillan@escuelalosandes.edu.ar>
2026-09-23 15:35:26 -0300

    etapa 2 incompleta

--- examen.py
+++ examen.py
@@ -27,7 +27,6 @@ tostados_c = 0
 nombres_p = ("Agua", "Alfajor", "Tostado")
 precio_p = (700, 900, 2200)
 
-
 nombre_c = input("ingrese su nombre: ")
 dinero = input("ingrese su dinero disponible: ")
 
@@ -46,14 +45,47 @@ print(f"""
 # Utilizar las listas para obtener producto y precio.
 
 print("""
+---PRODUCTOS---
 1. Agua       - $700
 2. Alfajor    - $900
 3. Tostado    - $2200
 4. Consultar pedido
 5. Finalizar compra
+
+por favor, seleccione una opción con los números.
 """)
+opcion = int(input(": "))
 
-
+if opcion<1 or opcion>5:
+    print("La opción seleccionada no se encuentra dentro de las opciónes. Por favor, vuelva a iniciar el programa.")
+elif opcion == 1:
+    print(f"{producto[0]}: ${precio[0]}")
+    
+    if dinero < (precio[0]):
+        print("Saldo insuficiente para realizar esta compra.")
+    else:
+        dinero = dinero - (precio[0])
+        aguas_c = aguas_c + 1
+        productos_c = productos_c + 1
+        dinero_g = dinero_g + (precio[0])
+        
+elif opcion == 2:
+    print(f"{producto[1]}: ${precio[1]}")
+elif opcion == 3:
+    print(f"{producto[2]}: ${precio[2]}")
+elif opcion == 4:
+    print(f"{producto[3]}: ${precio[3]}")
+elif opcion == 5:
+    print(f"{producto[4]}: ${precio[4]}")
+    
+    print (f"""{nombre_c}
+{dinero}
+{dinero_g}
+{productos_c}
+{aguas_c}
+{alfajor_c}
+{tostados_c}""")
+    
 # =========================
 # ETAPA 3 - CICLO PRINCIPAL
 # =========================
