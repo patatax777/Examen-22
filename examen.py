efcb15b30227f239a7620c03f9618ae3121f34d3
patatax777 <kmelillan@escuelalosandes.edu.ar>
2026-09-24 17:39:18 -0300

    2.2
    selección de opción 1: hecha
    
    corrección de visuales; corrección de variables y creación de nuevas

--- examen.py
+++ examen.py
@@ -4,10 +4,13 @@ product = {"Agua" : 700,
            "Alfajor" : 900,
            "Tostado" : 2200}
 """
+#creación de variavlea
 name = ["Agua", "Alfajor", "Tostado"]
 price = [700, 900, 2200]
 quantity = [0, 0, 0]
 products = sum(quantity)
+s_money = 0  
+option = 1
 
 """
 product ={
@@ -29,18 +32,21 @@ product ={
 
 print (product)
 """
+
+#pedido de datos y bienvenida
 print ("---KIOSKO PATATAX777---")
 client = input("ingrese su nombre: ")
-c_money = input("ingrese su dinero: ")
+c_money = int(input("ingrese su dinero: "))
 print ()
 input(f"""¡hola {client}!, estás ingresando al "KIOSKO PATATAX777".
 su saldo disponible es de: {c_money}
         
         presione "enter" para continuar--""")
-option = 1
+print()
+
+#empezando bucle kiosko patatax777 
 while option != 5:
-    print ("""
-    ---KIOSKO PATATAX777---
+    print ("""---KIOSKO PATATAX777---
     1. Agua       - $700
     2. Alfajor    - $900
     3. Tostado    - $2200
@@ -48,8 +54,31 @@ while option != 5:
     5. Finalizar compra
     """)
     option = int(input("por favor, seleccione una opción en NÚMEROS: "))
+    print()
+
     if option == 1:
-        print ("seleccionó 1")#postergado
+        print(f"""producto seleccionado: {name[0]}
+costo del producto: {price[0]}
+saldo actual: {c_money}""")
+
+        if c_money >= price[0]:
+            yes_no = int(input("""desea comprar este producto?
+            sí = 1
+            no = 0
+            -"""))
+            if yes_no == 1:
+                quantity[0] += 1
+                c_money -= price[0]
+                s_money += price[0]
+                print("compra exitosa, regresando al menú principal...")
+            elif yes_no == 0:
+                print("""compra cancelada, regresando al menú principal...""")
+            else:
+                print("""opción no valida, compra cancelada. regresando al menú principal...""")
+        else:
+            print("no tienes suficiente dinero para comprar este producto")
+            input("press enter")
+        print ("")
     elif option == 4:
         print()
     elif option == 5:
@@ -57,3 +86,4 @@ while option != 5:
     else:
         print("el número ingresado es inválido. por favor, seleccione un número que aparezca enn la tabla.")
         input("press enter")
+        #ya m canse wn quiero dibujar :w
